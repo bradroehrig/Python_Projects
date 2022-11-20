bf582cda390b1752d73b2a27008fbaf6096885ef
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2022-11-20 14:28:36 -0600

    Update practice.py

--- practice.py
+++ practice.py
@@ -174,24 +174,24 @@ my_toyota = Car()
 #You will see "building car"#printed.
 # ****************************************************************************
 class Dog:
- 
+
     # class attribute
     attr1 = "mammal"
     attr2 = "reptile" 
- 
+
     # Instance attribute
     def __init__(self, name):
         self.name = name
- 
+
 # Driver code
 # Object instantiation
 Rodger = Dog("Rodger")
 Tommy = Dog("Tommy")
- 
+
 # Accessing class attributes
 print("Rodger is a {}".format(Rodger.__class__.attr1))
 print("Tommy is a {}".format(Tommy.__class__.attr2))
- 
+
 # Accessing instance attributes
 print("My name is {}".format(Rodger.name))
 print("My name is {}".format(Tommy.name))
@@ -202,31 +202,31 @@ print("My name is {}".format(Tommy.name))
 # My name is Tommy
 # ****************************************************************************
 class Dog:
- 
+
 	# class attribute
 	attr1 = "mammal"
 	attr2 = "reptile" 
- 
+
 	# Instance attribute
 	def __init__(self, name, age):
 		self.name = name
 		self.age = age
- 
+
 # Driver code
 # Object instantiation
 Rodger = Dog("Rodger", 47)
 Tommy = Dog("Tommy", 23)
- 
+
 # Accessing class attributes
 print("Rodger is a {}".format(Rodger.__class__.attr1))
 print("Tommy is a {}".format(Tommy.__class__.attr2))
- 
+
 # Accessing instance attributes
 print("My name is {}".format(Rodger.name))
 print("and I'm {}.".format(Rodger.age))
 print("My name is {}".format(Tommy.name))
 print("and I'm {}.".format(Tommy.age))
-   
+
 # Rodger is a mammal# 
 # Tommy is a reptile
 # My name is Rodger
@@ -237,41 +237,40 @@ print("and I'm {}.".format(Tommy.age))
 # ****************************************************************************
 # Python code to demonstrate how parent constructors
 # are called.
- 
+
 # parent class
 class Person(object):
- 
+
     # __init__ is known as the constructor
     def __init__(self, name, idnumber):
         self.name = name
         self.idnumber = idnumber
- 
+
     def display(self):
         print(self.name)
         print(self.idnumber)
-         
+
     def details(self):
         print("My name is {}".format(self.name))
         print("IdNumber: {}".format(self.idnumber))
-     
+
 # child class
 class Employee(Person):
     def __init__(self, name, idnumber, salary, post):
         self.salary = salary
         self.post = post
- 
+
         # invoking the __init__ of the parent class
         Person.__init__(self, name, idnumber)
-         
+
     def details(self):
         print("My name is {}".format(self.name))
         print("IdNumber: {}".format(self.idnumber))
         print("Post: {}".format(self.post))
- 
- 
+
 # creation of an object variable or an instance
 a = Employee('Rahul', 886012, 200000, "Intern")
- 
+
 # calling a function of the class Person using
 # its instance
 a.display()
